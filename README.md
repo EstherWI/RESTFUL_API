68e6dccc4139d229a708d4261627a9a5798bc889
EstherWI <esthersss09@gmail.com>
2023-08-13 15:25:01 -0300

    add sqlite3 config and search users funcionality

--- README.md
+++ README.md
@@ -5,16 +5,29 @@ Backend for a web application that allows users to load a CSV file with preforma
 ## Requirements
 
 - [X] Upload a csv file
-- [ ] Search Users
+- [X] Search Users
 - [X] Node RESTful API
 - [ ] Errors handling
 - [ ] Automated Tests 
+
+## Endpoints
+- [X] [POST /api/files] An endpoint that accepts a CSV file upload from the frontend and stores the data in a database or a data structure. You should use the key "file" in the body request.
+- [X] [GET /api/users] Should include an endpoint that allows the frontend to search through the loaded CSV data. This route should accept a ?q= query parameter for search terms and should search through EVERY column of the CSV. The filter should search for partial matches and also be case insensitive.
+
+## Node Version
+- v18.16.1
 
 ## Install and Run
 
 ```properties
 npm install
 npm run dev
+```  
+
+## Testing
+
+```properties
+npm run test
 ```  
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import { Router, Request, Response } from 'express';
+import { User } from './types';
 import express from 'express';
 import multer from 'multer';
-import { Router, Request, Response } from 'express';
+import fs from 'fs';
+import csv from 'csv-parser';
+import sqlite3 from 'sqlite3';
 
 const app = express();
 const port = 3000;
@@ -8,20 +12,69 @@ const route = Router()
 
 app.use(express.json())
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
 const upload = multer({ storage });
+
+const db = new sqlite3.Database(':memory:'); 
+
+db.serialize(() => {
+    db.run('CREATE TABLE users (name TEXT,  city TEXT, country TEXT, favorite_sport TEXT)');
+});
 
 route.post('/api/files', upload.single('file'), (req: Request, res: Response) => {
     const file = req.file;
     if (!file) {
         return res.status(400).json({ error: 'No file uploaded.' });
     }
-    console.log(file)
-    return res.status(200).json({ message: 'File uploaded successfully.' });
+    const users: User[] = [];
+    try {
+        fs.createReadStream(file.path)
+            .pipe(csv())
+            .on('data', (user: User) => {
+                users.push(user);
+            })
+            .on('end', () => {
+                fs.unlinkSync(file.path);
+                db.serialize(() => {
+                    const stmt = db.prepare('INSERT INTO users VALUES (?, ?, ?, ?)');
+                    users.forEach(user => stmt.run(user.name, user.city, user.country, user.favorite_sport));
+
+                    stmt.finalize();
+                });
+                return res.status(200).json({ message: 'File uploaded successfully.' });
+            });
+
+    } catch (error) {
+        return res.status(400).json({ error: error });
+    }
 })
 
 route.get('/api/users', (req: Request, res: Response) => {
-    res.json({ message: 'users' })
+    const searchTerm = req.query.q;
+
+    let query = 'SELECT * FROM users';
+    const queryParams = [];
+
+    if (searchTerm && typeof searchTerm === 'string') {
+        query += ' WHERE LOWER(name || " " || country || " " || city || " " || favorite_sport || " ") LIKE ?';
+        queryParams.push(`%${searchTerm.toLowerCase()}%`);
+    }
+
+    db.all(query, queryParams, (err, rows) => {
+        if (err) {
+            console.error('SQLite Error:', err.message);
+            return res.status(500).json({ error: 'An error occurred while querying the database.' });
+        }
+        return res.status(200).json(rows);
+    });
+
 })
 
 app.use(route)
